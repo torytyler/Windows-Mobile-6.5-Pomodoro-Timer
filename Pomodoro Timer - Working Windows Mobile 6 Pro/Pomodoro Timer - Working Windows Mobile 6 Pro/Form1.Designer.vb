e72197a37c68121d48bbec41eb82fa488aa3ced8
Phakio <torytyler@icloud.com>
2024-04-19 00:48:13 -0400

    source codeアップロードする
    どうぞ〜

---
+++ Pomodoro Timer - Working Windows Mobile 6 Pro/Pomodoro Timer - Working Windows Mobile 6 Pro/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+    private mainMenu1 As System.Windows.Forms.MainMenu
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.mainMenu1 = New System.Windows.Forms.MainMenu
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.timerWork = New System.Windows.Forms.Timer
+        Me.timerBreak = New System.Windows.Forms.Timer
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Button1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Button1.Location = New System.Drawing.Point(84, 206)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(72, 20)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start!"
+        '
+        'timerWork
+        '
+        Me.timerWork.Interval = 1000
+        '
+        'timerBreak
+        '
+        Me.timerBreak.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("Arial", 28.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label1.Location = New System.Drawing.Point(62, 94)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 62)
+        Me.Label1.Text = "-.-"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(84, 229)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 39)
+        Me.Label2.Text = "made w/ luv  pko 2024"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(240, 268)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular)
+        Me.Menu = Me.mainMenu1
+        Me.Name = "Form1"
+        Me.Text = "Pomodoro Timer"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents timerWork As System.Windows.Forms.Timer
+    Friend WithEvents timerBreak As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Pomodoro Timer - Working Windows Mobile 6 Pro/Pomodoro Timer - Working Windows Mobile 6 Pro/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Private remainingWorkTime As Integer = 25 * 60 ' 25 minutes
+    Private remainingBreakTime As Integer = 5 * 60 ' 5 minutes
+    Private isTimerPaused As Boolean = False
+    Private isWorkTimerActive As Boolean = True
+
+    ' Declare constants
+    Private Const SND_SYNC As Integer = &H0 ' Play synchronously (the function will not return until the sound finishes playing)
+    Private Const SND_ASYNC As Integer = &H1 ' Play asynchronously
+    Private Const SND_FILENAME As Integer = &H20000 ' The parameter is a filename
+
+    ' Import the PlaySound function from coredll.dll
+    <DllImport("coredll.dll", EntryPoint:="PlaySound", SetLastError:=True)> _
+    Private Shared Function PlaySound(ByVal szSound As String, ByVal hMod As IntPtr, ByVal flags As Integer) As Boolean
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ' Check if any timer is currently active
+        If timerWork.Enabled Or timerBreak.Enabled Then
+            ' Pause the active timer
+            timerWork.Enabled = False
+            timerBreak.Enabled = False
+            isTimerPaused = True
+        Else
+            ' If the timer is paused, decide which timer to resume
+            If isTimerPaused Then
+                timerWork.Enabled = isWorkTimerActive
+                timerBreak.Enabled = Not isWorkTimerActive
+                isTimerPaused = False
+            Else
+                ' If no timer is paused, start the work timer
+                isWorkTimerActive = True ' Start with work timer
+                timerWork.Enabled = True
+                timerBreak.Enabled = False
+            End If
+        End If
+        UpdateTimerLabel(If(isWorkTimerActive, remainingWorkTime, remainingBreakTime))
+        PlayMySound("hai.wav") ' Adjust the sound file name as needed for this event
+    End Sub
+
+    Private Sub timerWork_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timerWork.Tick
+        If remainingWorkTime > 0 Then
+            remainingWorkTime -= 1
+            UpdateTimerLabel(remainingWorkTime)
+        Else
+            PlayMySound("hard.wav") ' Use appropriate sound for work timer end
+            ' Switch to break timer
+            isWorkTimerActive = False
+            timerWork.Enabled = False
+            timerBreak.Enabled = True
+            remainingWorkTime = 25 * 60 ' Reset for next cycle
+        End If
+    End Sub
+
+    Private Sub timerBreak_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timerBreak.Tick
+        If remainingBreakTime > 0 Then
+            remainingBreakTime -= 1
+            UpdateTimerLabel(remainingBreakTime)
+        Else
+            ' End of break timer, start vibration
+            PlayMySound("nice.wav")
+            ' Switch to work timer
+            isWorkTimerActive = True
+            timerBreak.Enabled = False
+            timerWork.Enabled = True
+            remainingBreakTime = 5 * 60 ' Reset for next cycle
+        End If
+    End Sub
+
+
+
+    Private Sub PlayMySound(ByVal soundFileName As String)
+        Dim basePath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)
+        If basePath.StartsWith("file:\\") Then
+            basePath = New Uri(basePath).LocalPath
+        End If
+        Dim soundPath As String = System.IO.Path.Combine(basePath, soundFileName)
+        System.Diagnostics.Debug.WriteLine("Attempting to play sound from: " + soundPath)
+        PlaySound(soundPath, IntPtr.Zero, SND_ASYNC Or SND_FILENAME)
+    End Sub
+
+
+
+
+    Private Sub UpdateTimerLabel(ByVal seconds As Integer)
+        Label1.Text = String.Format("{0}:{1:00}", seconds \ 60, seconds Mod 60)
+        Button1.Text = If(isTimerPaused Or (Not timerWork.Enabled And Not timerBreak.Enabled), "Start", "Pause")
+    End Sub
+
+
+End Class
